04664ff419bbc676c403bc240b4aebeb23d302c2
IlariaGalliano <ilaria.galliano7@gmail.com>
2020-10-15 19:20:11 +0200

    js-mail-dadi
    Esercizio 1 mail + Esercizio 2 completo dadi

--- js/main.js
+++ js/main.js
@@ -2,31 +2,36 @@
 * Es. 1 Mail
 *
 */
-/*
+
 // chiedere mail all'utente
 var mail = prompt('Inserire email')
 console.log(mail);
 
 // lista di chi può accedere
 var list = ['luca@gmail.com','marco@gmail.com','matteo@gmail.com','anna@gmail.com','lucia@gmail.com','salvatore@gmail.com','alessio@gmail.com','vanessa@gmail.com','adele@gmail.com','sofia@gmail.com','valentina@gmail.com','giovanni@gmail.com']
-//console.log(list);
+console.log(list);
+
+// Per la ricerca nell'arrey
+var emailFound = false;
 
 // ciclo
-for (var i = 0; i == mail; i++) {
-  var item = mail[i]
-if (item === list){
-  text.innerHTML = 'La tua mail è stata accettata ed è : ' + list;
-  console.log(item);
-}
-else {
-  text.innerHTML = 'La tua mail non è stata accettata';
-  console.log(item);
+for (var i = 0; i < list.length; i++) {
+  var item = list[i];
+if (mail === item) {
+  emailFound = true;
 }
 }
 
-risultato
+// Condizione
+if (emailFound == true) {
+  text.innerHTML = 'La tua mail è stata accettata ed è : ' + mail;
+} else {
+  text.innerHTML = 'La tua mail non è stata accettata ';
+}
+
+//risultato
 var result = document.getElementById('text');
-*/
+
 
 /*
 * Es. 2 Gioco dei dadi
@@ -38,10 +43,12 @@ var result = document.getElementById('text');
 //Giocatore
 var numberUser = parseInt( prompt('inserisci un numero random da 1 a 6 '));
 console.log(numberUser);
+
 //Computer
 var numberComputer = Math.floor(Math.random() * 6) + 1;;
 console.log(numberComputer);
 
+// Condizione
 if (numberUser > numberComputer){
   text.innerHTML = 'Ha vinto l\'utente';
   console.log('Hai vinto');
@@ -51,4 +58,5 @@ else {
   console.log('Hai perso');
 }
 
+//risultato
 var result = document.getElementById('text');
